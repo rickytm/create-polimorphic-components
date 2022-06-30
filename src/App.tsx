5d11abddacfa7399f1aad9a56ad09da6f91f7644
Ricardo Muñoz <MunozRic@protrans.com>
2022-06-29 20:23:28 -0500

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import "./styles.css";
-import { Text } from "./components";
+import './styles.css';
+import { Text } from './components';
+import React from 'react';
+
+const Emphasis = ({ children }: { children: React.ReactText }) => {
+    return (
+        <em style={{ background: 'yellow', color: 'black', fontSize: '40px' }}>
+            {children}
+        </em>
+    );
+};
 
 export default function App() {
-  return (
-    <div className="App">
-      <Text as="h1">Hello CodeSandbox</Text>
-      <Text as="h1">Hello CodeSandbox</Text>
-      <Text as="a" href="www.google.com">
-        TE LA COMES
-      </Text>
-      <Text>Hello world</Text>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Text as='h1'>Hello CodeSandbox</Text>
+            <Text as='h1' color='red' style={{ background: 'black' }}>
+                Hello CodeSandbox
+            </Text>
+            <Text as='a' href='www.google.com'>
+                TE LA COMES
+            </Text>
+            <Text>Hello world</Text>
+            <Text data-props={'rico y sabroso'}>Span normal</Text>
+            <Text as='div' className='container'>
+                Div
+            </Text>
+            <Text as='p' style={{ color: 'blue', background: 'violet' }}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Maiores quam minima provident exercitationem iste obcaecati quas
+                recusandae eaque tenetur laudantium delectus, inventore quasi?
+                Iusto voluptate accusamus repellat commodi sunt dolorem minus
+                fuga! Laborum beatae, alias accusantium rerum tenetur aut
+                nostrum commodi, neque mollitia maiores autem. Voluptatibus
+                mollitia praesentium dignissimos excepturi!
+            </Text>
+            <Text as='button' color='red'>
+                Button
+            </Text>
+
+            <Text as={Emphasis}>This is important. You are so uggly</Text>
+        </div>
+    );
 }
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,13 +1,45 @@
-import React from "react";
-type TextProps<C extends React.ElementType> = {
-  as?: C;
-  children: React.ReactNode;
-} & React.ComponentPropsWithRef<C>;
+import React from 'react';
 
-export const Text = <C extends React.ElementType>({
-  as,
-  children
-}: TextProps<C>) => {
-  const Component = as || "span";
-  return <Component>{children}</Component>;
+type Rainbow =
+    | 'red'
+    | 'orange'
+    | 'yellow'
+    | 'green'
+    | 'blue'
+    | 'indigo'
+    | 'violet';
+
+type AsProp<C extends React.ElementType> = {
+    as?: C;
+};
+
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+type PropsWithAs<C extends React.ElementType,Props>=Props&AsProp<C>;
+
+type PolymorphicComponentProps<
+    C extends React.ElementType,
+    Props = {}
+> = React.PropsWithChildren<Props & AsProp<C>> &
+    Omit<React.ComponentPropsWithRef<C>, PropsToOmit<C, Props>>;
+
+type TextProps = { color?: Rainbow | 'black' };
+
+export const Text = <C extends React.ElementType = 'span'>({
+    as,
+    color,
+    style,
+    children,
+    ...restProps
+}: PolymorphicComponentProps<C, TextProps>) => {
+    const internalStyles = color
+        ? { style: { ...style, color } }
+        : style
+        ? { style: { ...style } }
+        : {};
+    const Component = as || 'span';
+    return (
+        <Component {...restProps} {...internalStyles}>
+            {children}
+        </Component>
+    );
 };
